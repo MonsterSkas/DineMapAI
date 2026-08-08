d5286c73527c1da9913b3649c1f99380e9c4fd92
CretivityArt <117714572+CretivityArt@users.noreply.github.com>
2026-08-08 13:32:40 +0530

    Add files via upload

---
+++ backend/services/overpass_resturant.py
@@ -0,0 +1,56 @@
+import requests
+
+
+OVERPASS_URL = "https://overpass-api.de/api/interpreter"
+
+
+def get_restaurants(latitude, longitude, radius=2000):          #5km
+
+    query = f"""
+    [out:json][timeout:30];
+
+    nwr["amenity"="restaurant"]
+    (around:{radius},{latitude},{longitude});
+
+    out center;
+    """
+
+    headers = {
+        "User-Agent": "DineMapAI/1.0 (hackathon project)"
+    }
+
+    response = requests.post(
+        OVERPASS_URL,
+        data=query,
+        headers=headers,
+        timeout=60
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    restaurants = []
+
+    for element in data.get("elements", []):
+
+        tags = element.get("tags", {})
+
+        lat = element.get("lat")
+        lon = element.get("lon")
+
+        if lat is None or lon is None:
+            center = element.get("center", {})
+            lat = center.get("lat")
+            lon = center.get("lon")
+
+        if lat is not None and lon is not None:
+            restaurants.append({
+                "id": element["id"],
+                "name": tags.get("name", "Unnamed Restaurant"),
+                "latitude": lat,
+                "longitude": lon,
+                "tags": tags
+            })
+
+    return restaurants
